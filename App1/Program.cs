9e2aa45baa610717bdf1422b7153e4d86dc20cd9
alber <alber@ALBERTO_BARBOSA>
2022-07-15 09:52:21 -0300

    Adicionar arquivos de projeto.

---
+++ App1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace App1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculando Trapézio
+
+            double b1, b2, h, area;//a letra a e b são as bases, e o h é a altura e a area será o resultado
+            b1 = 6.0;
+            b2 = 8.0;
+            h = 5.0;
+
+            area = (b1 + b2) / 2.0 * h;
+
+            Console.WriteLine("A area do trapésio é = " + area);
+        }
+    }
+}
